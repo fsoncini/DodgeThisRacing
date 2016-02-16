abf2edff97f87fd115126066bf7fb574ba19e53f
BaggioPereira <baggio_111111@hotmail.com>
2016-02-16 15:57:31 +0000

    Wheel meshs update

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -11,7 +11,8 @@
     }
          
     public class CarController : MonoBehaviour {
-        public List<AxleInfo> axleInfos; 
+        public List<AxleInfo> axleInfos;
+        public GameObject[] wheels = new GameObject[4];
         public float maxMotorTorque;
         public float maxSteeringAngle;
          
@@ -32,7 +33,22 @@
             visualWheel.transform.position = position;
             visualWheel.transform.rotation = rotation;
         }
-         
+
+        public void updateMesh()
+        {
+            for(int i = 0; i < 4 ; i++)
+            {
+                Quaternion quat;
+                Vector3 pos;
+
+                wheels[i].GetComponent<WheelCollider>().GetWorldPose(out pos, out quat);
+                quat *= Quaternion.Euler(0, 0, 90);
+
+                //wheels[i].transform.position = pos;
+                wheels[i].transform.rotation = quat;
+            }
+        }
+
         public void FixedUpdate()
         {
             float motor = maxMotorTorque * Input.GetAxis("Vertical");
@@ -49,6 +65,7 @@
                 }
                 ApplyLocalPositionToVisuals(axleInfo.leftWheel);
                 ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+                updateMesh();
             }
         }
     }
